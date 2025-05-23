6bbfba2c119b99807e41b462da64eda2c69ec31a
xuyi <2217021563@qq.com>
2025-05-23 19:30:46 +0800

    优化目录导航和页面功能：1. 修复重复的回到顶部按钮问题 2. 解决目录区域双滚动条问题 3. 优化目录导航样式和交互

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import DocItem from '@theme-original/DocItem';
 import Comments from '@site/src/components/Comments';
-import BackToTop from '@site/src/components/BackToTop';
 
 export default function DocItemWrapper(props) {
   return (
     <>
       <DocItem {...props} />
       <Comments />
-      <BackToTop />
     </>
   );
 }
